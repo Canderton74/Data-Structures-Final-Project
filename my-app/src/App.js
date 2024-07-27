02f92e8734846a1145cc9ec7e822f10cfa513671
Cole <emerinecole@ufl.edu>
2024-07-27 16:05:50 -0400

    front end set up and basics, error testing - used react/node.js npm

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserInterface from './UserInterface';
+
+function App() {
+  return (
+    <div className="App">
+      <UserInterface />
+    </div>
+  );
+}
+
+export default App;
